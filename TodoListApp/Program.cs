093f278015162ead05ca1a27da483b506f76adb9
GlebTsoy2007 <glebtsoy7@gmail.com>
2026-09-15 13:48:50 +0300

    lesson 2: add survey age

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Сделал Цой Глеб, Миллер Александр");
+﻿Console.WriteLine("Введите имя:");
+var name = Console.ReadLine();
+Console.Write("Введите фамилию: ");
+var surname = Console.ReadLine();
+Console.Write("Введите год рождения: ");
+var age = 2026 - int.Parse(Console.ReadLine());
+
+Console.Write($"Добавлен пользователь {name} {surname}, возраст: {age}");
