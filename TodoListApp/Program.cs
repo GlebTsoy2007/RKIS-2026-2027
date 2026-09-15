740ff8cd92daefe45c24cf1c714282a24bccf210
GlebTsoy2007 <glebtsoy7@gmail.com>
2026-09-15 14:04:09 +0300

    lesson 2: add survey age with verification int

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -1,8 +1,17 @@
-﻿Console.WriteLine("Введите имя:");
+﻿Console.Write("Введите имя: ");
 var name = Console.ReadLine();
 Console.Write("Введите фамилию: ");
 var surname = Console.ReadLine();
-Console.Write("Введите год рождения: ");
-var age = 2026 - int.Parse(Console.ReadLine());
-
-Console.Write($"Добавлен пользователь {name} {surname}, возраст: {age}");
+//цикл для правильного ввода числа
+while (true) {
+    Console.Write("Введите год рождения: ");
+    var age = Console.ReadLine();
+    if (int.TryParse(age, out int nage)){ 
+        Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст: {2026-nage}");
+        break;
+    }
+    else
+    {   
+        Console.WriteLine("Введитп  число в дату рождения!");
+    }
+}
